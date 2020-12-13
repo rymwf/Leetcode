6a91bba92623114c5a0e4c333913c2ae11f0bf15
Yzs <yangzs8888@hotmail.com>
2020-12-14 00:51:10 +0800

    add 49

---
+++ 49.GroupAnagrams.cpp
@@ -0,0 +1,57 @@
+/*
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Example 2:
+
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+    1 <= strs.length <= 104
+    0 <= strs[i].length <= 100
+    strs[i] consists of lower-case English letters.
+
+*/
+#include"common.h"
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ret;
+        unordered_map<string,int> mymap;
+        for(auto& str:strs)
+        {
+            string a(26, '0');
+            for (auto c : str)
+                a[c - 'a']++;
+            if(mymap.find(a)==mymap.end()){
+                mymap[a]=mymap.size();
+                ret.emplace_back(vector<string>{str});
+            }else{
+                ret[mymap[a]].emplace_back(str);
+            }
+        }
+        return ret;
+    }
+};
+int main(){
+    vector<string> strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    Solution s;
+}
