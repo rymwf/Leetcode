94ada34bb559ed06076fabfe2803062f21f84416
Yzs <yangzs8888@hotmail.com>
2021-01-02 17:47:00 +0800

    add 327

--- 327.CountofRangeSum.cpp
+++ 327.CountofRangeSum.cpp
@@ -21,10 +21,78 @@ Constraints:
 
 class Solution {
 public:
+    //exceed time limit
     int countRangeSum(vector<int>& nums, int lower, int upper) {
-        int ret=0;
-        int temp=0;
-        for(int i=0;i<nums.size();++i){
+        int len = nums.size();
+        if (len == 0)
+            return 0;
+        int ret{};
+        for (int i = 0; i < len; ++i)
+        {
+            long long temp{};
+            for (int j = i; j < len; ++j)
+            {
+                temp += nums[j];
+                if (temp <= upper && temp >= lower)
+                    ret++;
+            }
         }
+        return ret;
+    }
+
+    //slow
+    int countRangeSum2(vector<int>& nums, int lower, int upper) {
+        int len = nums.size();
+        if (len == 0)
+            return 0;
+        using ll=long long;
+        vector<ll> temp(len + 1);
+        temp[0]=0;
+        multimap<ll, int> mymap;
+        for (int i = 0; i < len; ++i)
+        {
+            temp[i + 1] = temp[i] + nums[i];
+            mymap.emplace(temp[i + 1], i);
+        }
+        int ret{};
+        for (int i = 0; i < len; ++i)
+        {
+            ll a = lower + temp[i];
+            ll b = upper + temp[i];
+            ////for (auto it = mymap.lower_bound(a); it != mymap.end() && it->first <= b; ++it)
+            for (auto it = mymap.lower_bound(a), it0 = mymap.upper_bound(b); it != it0; ++it)
+            {
+                if (it->second >= i)
+                    ret++;
+            }
+        }
+        return ret;
+    }
+    int countRangeSum3(vector<int>& nums, int lower, int upper) {
+ //       int len = nums.size();
+ //       if (len == 0)
+ //           return 0;
+ //       using ll=long long;
+ //       vector<ll> numsum(nums.begin(),nums.end());
+ //       vector<pair<ll,int>> temp(nums.size());
+ //       temp[0] = {nums[0], 0};
+ //       for(int i=1;i<len;++i)
+ //       {
+ //           numsum[i] += numsum[i - 1];
+ //           temp[i] = {numsum[i], i};
+ //       }
+ //       sort(temp.begin(),temp.end());
+////        static auto findlowerbound=[&nums](int a){
+////            
+////        };
+
+ //       return 0;    
     }
 };
+
+int main(){
+    vector<int> nums = {0, -1, -2, -3, 0, 2};
+    int lower = 3, upper = 5;
+    Solution s;
+    s.countRangeSum2(nums, lower, upper);
+}
